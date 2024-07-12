dba91d4a9bbe055f8cb58ffab05e7932898a61fa
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-07-12 10:26:38 +0300

    Add files via upload

---
+++ src/comp/delbin.bat
@@ -0,0 +1,16 @@
+@echo off
+rem *******************************************************************
+rem delbin.bat
+rem 	delete temporary working files
+rem
+rem Copyright (C) Aleksandr Bocharov
+rem Author: Aleksandr Bocharov
+rem Date: 12.07.2024
+rem *******************************************************************
+
+cd  src\comp\
+del *.asm 
+del *.o 
+del *.log
+
+
