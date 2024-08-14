3957364516d95ec47532da77025fb6eef8bac632
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-08-14 16:25:21 +0300

    Add files via upload

---
+++ r2dclg_dbg.bat
@@ -0,0 +1,18 @@
+@echo off
+rem *******************************************************************
+rem r2dclg_dbg.bat
+rem	compile from refal to GNU-assembler + debugger link + go
+rem 	arg: %1 - name of refal programm (without ext ref)
+rem	result: running of %1.exe in debugging mode 
+rem
+rem Copyright(C) Aleksandr Bocharov
+rem Author : Aleksandr Bocharov
+rem Date : 05.08.2024
+rem *******************************************************************
+
+set CC=F:\Programming\mingw32\bin
+
+bin\refal2 %1 > %1.log
+%CC%\as -o %1.o %1.asm >> %1.log
+%CC%\gcc -o %1.exe lib\rfdbg.o -Wl,-L.\lib %1.o -Wl,-lr2_dbg >> %1.log
+%1.exe
