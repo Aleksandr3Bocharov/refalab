0e6a63be31336560590580db53fd887547ddf709
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-08-28 16:36:10 +0300

    Delete cerr.h

--- cerr.h
+++
@@ -1,9 +0,0 @@
-#ifndef CERR_H
-#define CERR_H
-
-extern void pchosa(const char *s, char c);
-extern void pchosh(const char *s);
-extern void pchosj(const char *s, const char *sid, size_t lsid, const char *s1);
-//extern void pchosx(const char *s, const char *sid, size_t lsid, const char *s1);
-
-#endif
