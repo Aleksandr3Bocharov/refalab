3a26c672ba23db165abb58ad614fcd7a3744ca84
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-25 18:50:11 +0300

    Update RefalABdclg_dbg.bat

--- RefalABdclg_dbg.bat
+++ RefalABdclg_dbg.bat
@@ -1,4 +1,11 @@
 @echo off
+
+rem Copyright 2024 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2024-09-25
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
 rem *******************************************************************
 rem RefalABdclg_dbg.bat
 rem	compile from refal to GNU-assembler + debugger link + go
