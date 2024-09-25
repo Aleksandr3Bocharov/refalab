11cb808cbabd1546f0a8b088929d69fcbc5c7800
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-25 18:49:36 +0300

    Update and rename r2dclg_dbg.bat to RefalABdclg_dbg.bat

--- RefalABdclg_dbg.bat
+++ RefalABdclg_dbg.bat
@@ -1,16 +1,12 @@
 @echo off
 rem *******************************************************************
-rem r2dclg_dbg.bat
+rem RefalABdclg_dbg.bat
 rem	compile from refal to GNU-assembler + debugger link + go
 rem 	arg: %1 - name of refal programm (without ext ref)
 rem	result: running of %1.exe in debugging mode 
-rem
-rem Copyright(C) Aleksandr Bocharov
-rem Author : Aleksandr Bocharov
-rem Date : 05.08.2024
 rem *******************************************************************
 
-bin\refal2 %1 > %1.log
+bin\RefalAB %1 > %1.log
 as -o %1.o %1.asm >> %1.log
-gcc -o %1.exe lib\rfdbg.o -Wl,-L.\lib %1.o -Wl,-lr2_dbg >> %1.log
+gcc -o %1.exe lib\rfdbg.o -Wl,-L.\lib %1.o -Wl,-lRefalAB_dbg >> %1.log
 %1.exe
