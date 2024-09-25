02247d49f8ed64eace450dfe5f410ae91b249575
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-25 18:39:40 +0300

    Update and rename r2clg.bat to RefalABclg.bat

---
+++ RefalABclg.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem Copyright 2024 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2024-09-25
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
+rem *******************************************************************
+rem RefalABclg.bat
+rem	compile from refal to GNU-assembler + link + go
+rem 	arg: %1 - name of refal programm (without ext ref)
+rem	result: running of %1.exe 
+rem *******************************************************************
+
+bin\RefalAB %1 > %1.log
+as -o %1.o %1.asm >> %1.log
+gcc -o %1.exe lib\mainrf.o %1.o -Wl,-L.\lib -Wl,-lRefalAB >> %1.log
+%1.exe
