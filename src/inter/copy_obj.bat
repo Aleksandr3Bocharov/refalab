e6957204eb84b6b927ffb28f729909d4c7bc1ad3
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-25 19:11:01 +0300

    Update copy_obj.bat

--- src/inter/copy_obj.bat
+++ src/inter/copy_obj.bat
@@ -1,11 +1,14 @@
 @echo off
+
+rem Copyright 2024 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2024-09-25
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
 rem *******************************************************************
 rem copy_obj.bat
 rem 	copy mainrf.o and rfdbg.o
-rem
-rem Copyright (C) Aleksandr Bocharov
-rem Author: Aleksandr Bocharov
-rem Date: 12.07.2024
 rem *******************************************************************
 
 copy /y src\inter\mainrf.o lib\
