f8c1b6c5741fa9a4b46d0487e85c079bd3e962dc
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-26 22:10:37 +0300

    Update cs.c

--- src/comp/cs.c
+++ src/comp/cs.c
@@ -1,7 +1,8 @@
+
 //-------------  file  --  CS.C  ---------------
 //         Work with identifier table
-//        Last edition date : 11.07.24
 //----------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
