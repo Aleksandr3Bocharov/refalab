b06210b69488440ca6f2770d69a914b6af00f722
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-26 21:34:37 +0300

    Update ccst.c

--- src/comp/ccst.c
+++ src/comp/ccst.c
@@ -1,7 +1,8 @@
+
 //----------   file  CCST1.C  --------------
 //      Compiler file
-//      Last edition date : 11.07.24
 //------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
