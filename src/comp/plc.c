43fe192c9a9402faf463cfab3d29cb2dec719559
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-26 22:11:13 +0300

    Update plc.c

--- src/comp/plc.c
+++ src/comp/plc.c
@@ -1,7 +1,8 @@
+
 //--------------------  file  --  PLC.C --------------------
 //     standard PL/I-functions implemented on C-language
-//                Last edition date : 11.07.24
 //----------------------------------------------------------
+
 #include <stdlib.h>
 #include <stdint.h>
 #include "plc.h"
