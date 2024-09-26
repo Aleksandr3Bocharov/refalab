0c22758c655d8f800e7387d997319ec34b630827
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-26 22:28:29 +0300

    Update xcf.c

--- src/inter/xcf.c
+++ src/inter/xcf.c
@@ -1,7 +1,8 @@
+
 //------------ file -- XCF.C ---------------
 //   MO: ftochar, rftime, functab, chartof
-//       Last edition date : 11.07.24
 //------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
