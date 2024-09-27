b94d402e074157730b3ad7dea42992b06e3ef1a0
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-27 13:57:31 +0300

    Update cerr.c

--- src/comp/cerr.c
+++ src/comp/cerr.c
@@ -1,3 +1,8 @@
+// Copyright 2024 Aleksandr Bocharov
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+// 2024-09-27
+// https://github.com/Aleksandr3Bocharov/RefalAB
 
 //-------------  file  --  CERR.C  --------------- 
 //               Print error file                           
