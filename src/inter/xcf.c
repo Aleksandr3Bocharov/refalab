e1f3b1ff1e86d210165bc852e5fcdeb36832dad7
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-05 17:50:25 +0300

    Add files via upload

--- src/inter/xcf.c
+++ src/inter/xcf.c
@@ -1,11 +1,11 @@
 // Copyright 2024 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-09-27
+// 2024-10-05
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //------------ file -- XCF.C ---------------
-//   MO: ftochar, rftime, functab, chartof
+//   MO: ftochar, functab, chartof
 //------------------------------------------
 
 #include <stdio.h>
