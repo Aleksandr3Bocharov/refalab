48da4d5f3e6e058baa48f40a2b754065c6915a6f
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-19 20:37:58 +0300

    Add files via upload

--- RefalABdclg.bat
+++ RefalABdclg.bat
@@ -3,7 +3,7 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-09-25
+rem 2024-10-19
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
@@ -15,5 +15,5 @@ rem *******************************************************************
 
 bin\RefalAB %1 > %1.log
 as -o %1.o %1.asm >> %1.log
-gcc -o %1.exe lib\rfdbg.o -Wl,-L.\lib %1.o -Wl,-lRefalAB >> %1.log
+clang -o %1.exe lib\rfdbg.o -Wl,-L.\lib %1.o -Wl,-lRefalAB >> %1.log
 %1.exe
