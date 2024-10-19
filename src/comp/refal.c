29318ea5fe6d4e637a51a7e8407e5f5b30cd02f6
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-19 22:37:10 +0300

    Add files via upload

--- src/comp/refal.c
+++ src/comp/refal.c
@@ -150,7 +150,7 @@ FILE *systxt; // for module names
 uint32_t nommod;
 char parm_i[40]; // sourse file name
 // Aleksandr Bocharov   // compiler version
-const char vers_i[] = "RefalAB Version 0.1.3-beta 20241017 (c) Aleksandr Bocharov";
+const char vers_i[] = "RefalAB Version 0.2.0-beta 20241019 (c) Aleksandr Bocharov";
 char mod_i[13]; // 8+4+1 (xxxxxxxx.yyy0)
 
 static FILE *sysin;
