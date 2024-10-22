f10bec4bfaf82514573782a82617a9d430e2400a
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-22 16:11:17 +0300

    Add files via upload

---
+++ set_env.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Copyright 2024 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2024-10-22
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
+rem *******************************************************************
+rem set_env.bat
+rem	Set Environments Variables for RefalAB
+rem *******************************************************************
+
+setx REFALABBIN %~dp0\bin
+setx REFALABLIB %~dp0\lib
