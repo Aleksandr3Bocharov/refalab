4245eb33957fa3fe11a8640aad6c6e270c7a3090
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-27 19:02:53 +0200

    Add files via upload

--- src/comp/cj.c
+++ src/comp/cj.c
@@ -1,7 +1,7 @@
 // Copyright 2024 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-10-19
+// 2024-10-27
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //-----------------  file  --  cj.C  -------------------
@@ -90,7 +90,7 @@ static uint16_t delta;
 
 static void oshex(void)
 {
-    fputs("\nOSHEX: no memory!!!", stdout);
+    fputs("\nOSHEX: no memory!!!\n", stdout);
     exit(1);
     return;
 }
--- src/comp/clu.c
+++ src/comp/clu.c
@@ -1,7 +1,7 @@
 // Copyright 2024 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-09-27
+// 2024-10-27
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //----------------  file  --  CLU.C  -------------------
@@ -21,7 +21,7 @@ static T_U *korenj = NULL; // tree koren
 
 void uns_sto(void)
 {
-    printf("\nNo memory for identifier table");
+    printf("\nNo memory for identifier table\n");
     exit(1);
     return;
 }
