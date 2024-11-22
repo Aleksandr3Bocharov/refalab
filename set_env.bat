b5149dd69793545e62ceb24a7dd2e12bd4990122
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-11-22 20:20:10 +0200

    Update set_env.bat

--- set_env.bat
+++ set_env.bat
@@ -3,12 +3,12 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-10-22
+rem 2024-11-23
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
 rem set_env.bat
-rem	Set Environments Variables for RefalAB
+rem Set Environments Variables for RefalAB
 rem *******************************************************************
 
 setx REFALABBIN %~dp0bin
