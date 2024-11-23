0ccbc40f394de0d90ed1a2f023b6dea9432b275f
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-11-23 16:00:46 +0200

    Update refxcv.bat

--- src/inter/refxcv.bat
+++ src/inter/refxcv.bat
@@ -3,14 +3,14 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-09-25
+rem 2024-11-23
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
 rem refxcv.bat
-rem 	special script
-rem	compile xcv.ref from refal to GNU-assembler + assembling. 
-rem	result: xcv.o  
+rem special script
+rem compile xcv.ref from refal to GNU-assembler + assembling
+rem result: xcv.o  
 rem *******************************************************************
 
 bin\RefalAB src\inter\xcv > src\inter\xcv.log
