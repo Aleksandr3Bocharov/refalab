47b664d188599ffbe78279c592e711a677e2d3fd
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-11-23 15:59:13 +0200

    Update refplatform.bat

--- src/inter/refplatform.bat
+++ src/inter/refplatform.bat
@@ -3,14 +3,14 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-09-30
+rem 2024-11-23
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
 rem refplatform.bat
-rem 	special script
-rem	compile xplatformwin.ref from refal to GNU-assembler + assembling. 
-rem	result: xplatform.o  
+rem special script
+rem compile xplatformwin.ref from refal to GNU-assembler + assembling. 
+rem result: xplatform.o  
 rem *******************************************************************
 
 bin\RefalAB src\inter\xplatformwin > src\inter\xplatform.log
