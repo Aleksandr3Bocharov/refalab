76d10a79b02f24c30c92512db0f027a89431754f
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-01-02 08:02:03 +0200

    Add files via upload

--- src/inter/xtime.c
+++ src/inter/xtime.c
@@ -1,7 +1,7 @@
 // Copyright 2024 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-10-28
+// 2025-01-02
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //-------------- file -- XTIME.C ------------
@@ -49,46 +49,45 @@ void (*rftime_1)(void) = rftime_;
 static void rftm_(void)
 {
     T_LINKCB *p = refal.preva->next;
-    if (p != refal.nexta)
-        if (p->tag == TAGO && p->next == refal.nexta)
+    if (p->next == refal.nexta && p->tag == TAGO)
+    {
+        const char c = p->info.infoc;
+        switch (c)
         {
-            const char c = p->info.infoc;
-            switch (c)
+        case 's':
+        case 'S':
+            timespec_get(&t0, TIME_UTC);
+            return;
+        case 'g':
+        case 'G':
+            timespec_get(&t1, TIME_UTC);
+            int32_t in = (int32_t)(t1.tv_nsec - t0.tv_nsec);
+            uint32_t is = (uint32_t)difftime(t1.tv_sec, t0.tv_sec);
+            if (in < 0)
             {
-            case 's':
-            case 'S':
-                timespec_get(&t0, TIME_UTC);
-                return;
-            case 'g':
-            case 'G':
-                timespec_get(&t1, TIME_UTC);
-                int32_t in = (int32_t)(t1.tv_nsec - t0.tv_nsec);
-                uint32_t is = (uint32_t)difftime(t1.tv_sec, t0.tv_sec);
-                if (in < 0)
-                {
-                    in += 1000000000;
-                    is--;
-                }
-                uint32_t im = is / 60;
-                is %= 60;
-                const uint32_t ih = im / 60;
-                im %= 60;
-                char s[30];
-                sprintf(s, "%02u:%02u:%02u.%09d", ih, im, is, in);
-                p = refal.prevr;
-                if (!slins(p, strlen(s)))
-                    return;
-                for (size_t i = 0; s[i] != '\0'; i++)
-                {
-                    p = p->next;
-                    p->tag = TAGO;
-                    p->info.codep = NULL;
-                    p->info.infoc = s[i];
-                }
-                return;
-            default:;
+                in += 1000000000;
+                is--;
             }
+            uint32_t im = is / 60;
+            is %= 60;
+            const uint32_t ih = im / 60;
+            im %= 60;
+            char s[30];
+            sprintf(s, "%02u:%02u:%02u.%09d", ih, im, is, in);
+            p = refal.prevr;
+            if (!slins(p, strlen(s)))
+                return;
+            for (size_t i = 0; s[i] != '\0'; i++)
+            {
+                p = p->next;
+                p->tag = TAGO;
+                p->info.codep = NULL;
+                p->info.infoc = s[i];
+            }
+            return;
+        default:;
         }
+    }
     refal.upshot = 2;
     return;
 }
