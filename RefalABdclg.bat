bcd6bfcd6b705ed8bc2bccf0ed08f602e3a92894
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-02-03 19:16:03 +0200

    Add files via upload

--- RefalABdclg.bat
+++ RefalABdclg.bat
@@ -1,16 +1,16 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-11-23
+rem 2025-02-03
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
 rem RefalABdclg.bat
 rem compile from refal to GNU-assembler + debugger link + go
 rem arg: %1 - name of refal programm (without ext ref)
-rem result: running of %1.exe in debugging mode 
+rem result: running of %1.exe debugger 
 rem *******************************************************************
 
 bin\RefalAB %1 > %1.log
