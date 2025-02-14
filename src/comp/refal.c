374781a9b19f0ab215862ce0fbd68e1778fc2df8
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-02-14 18:48:57 +0200

    Add files via upload

--- src/comp/refal.c
+++ src/comp/refal.c
@@ -1,11 +1,11 @@
 // Copyright 2025 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-02-08
+// 2025-02-14
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //-----------  file  --  REFAL.C -------------
-//      The main file of refal compiler
+//      The main file of RefalAB compiler
 //--------------------------------------------
 
 #include <stdio.h>
