3c89952c321cd381d5947c67154f6e8101903ec6
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-10 14:42:44 +0200

    Add files via upload

--- src/inter/refplatform.bat
+++ src/inter/refplatform.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-11-23
+rem 2025-03-10
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
@@ -14,4 +14,4 @@ rem result: xplatform.o
 rem *******************************************************************
 
 bin\RefalAB src\inter\xplatformwin > src\inter\xplatform.log
-as -o src\inter\xplatform.o src\inter\xplatformwin.asm >> src\inter\xplatform.log 
+as -o src\inter\xplatform.o src\inter\xplatformwin.s >> src\inter\xplatform.log 
--- src/inter/refxcv.bat
+++ src/inter/refxcv.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-11-23
+rem 2025-03-10
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
@@ -14,4 +14,4 @@ rem result: xcv.o
 rem *******************************************************************
 
 bin\RefalAB src\inter\xcv > src\inter\xcv.log
-as -o src\inter\xcv.o src\inter\xcv.asm >> src\inter\xcv.log 
+as -o src\inter\xcv.o src\inter\xcv.s >> src\inter\xcv.log 
