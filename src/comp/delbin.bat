65975667746cb13a1981c6737e31fd4859ec032f
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-10 14:35:11 +0200

    Add files via upload

--- src/comp/delbin.bat
+++ src/comp/delbin.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-11-23
+rem 2025-03-10
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
@@ -12,7 +12,7 @@ rem delete temporary working files
 rem *******************************************************************
 
 cd  src\comp\
-del *.asm 
+del *.s 
 del *.o 
 del *.log
 
