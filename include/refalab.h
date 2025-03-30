6df87413ece1deb5ef3530c14511ce7c9fc00b01
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-30 21:34:45 +0300

    Add files via upload

--- include/refalab.h
+++ include/refalab.h
@@ -1,7 +1,7 @@
 // Copyright 2025 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-03-23
+// 2025-03-30
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //--------------- file -- REFALAB.H -----------------
@@ -110,6 +110,7 @@ extern void rfexec(uint8_t *func);
 extern void rfinit(void);
 extern void rfpex(const char *pt, const T_LINKCB *pr, const T_LINKCB *pn);
 extern void rfpexm(const char *pt, const T_LINKCB *pr, const T_LINKCB *pn);
+extern void rfgetargs(int argc, char *argv[]);
 extern void rftermm(void);
 extern void rftpl(T_LINKCB *r, T_LINKCB *p, T_LINKCB *q);
 
