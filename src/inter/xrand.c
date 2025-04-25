a5d34b7383ba368f198a672ed48d0036664949f6
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-04-25 14:07:44 +0300

    Add files via upload

--- src/inter/xrand.c
+++ src/inter/xrand.c
@@ -5,9 +5,29 @@
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //-------------- file -- XRAND.C ------------
-//        MO: random
+//        MO: random,
 //            random_number
 //-------------------------------------------
 
+//#include <stdio.h>
+#include <stdint.h>
+#include "refalab.h"
+#include "rfintf.h"
+
+static void random_(void)
+{
+    return;
+}
+char random_0[] = {Z6 'R', 'A', 'N', 'D', 'O', 'M', (char)6};
+G_L_B uint8_t refalab_random = '\122';
+void (*random_1)(void) = random_;
+
+static void random_number_(void)
+{
+    return;
+}
+char random_number_0[] = {Z5 'R', 'A', 'N', 'D', 'O', 'M', '_', 'N', 'U', 'M', 'B', 'E', 'R', (char)13};
+G_L_B uint8_t refalab_random_number = '\122';
+void (*random_number_1)(void) = random_number_;
 
 //--------------- end of file  XRAND.C -------------
