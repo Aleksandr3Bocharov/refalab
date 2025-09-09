0662e7d229243608cf28fedaba15aebc2a2fc2c9
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-09-09 18:52:07 +0300

    Add files via upload

--- src/inter/refalab.h
+++ src/inter/refalab.h
@@ -1,7 +1,7 @@
 // Copyright 2025 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-04-28
+// 2025-09-09
 // https://github.com/Aleksandr3Bocharov/RefalAB
 
 //--------------- file -- REFALAB.H -----------------
@@ -43,7 +43,7 @@
 #define LBLL sizeof(void *)
 #define SMBL (ZBLL + LBLL)
 
-#define MAX_NUMBER 0xFFFFFF
+#define MAX_NUMBER 0xFFFFFFFFLL
 
 // pragma for 1 byte aligniment structure ------------
 #pragma pack(1)
