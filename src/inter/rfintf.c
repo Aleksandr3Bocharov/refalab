0fff7ba7eaa0e2b7b2d7fb7df2738e97b25c131a
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-03 21:33:34 +0300

    Add files via upload

--- src/inter/rfintf.c
+++ src/inter/rfintf.c
@@ -414,7 +414,7 @@ void rfpex(const char *pt, const T_LINKCB *pr, const T_LINKCB *pn, const bool nl
         }
         else if (pr->tag == TAGR)
             printf("'%%%p'", (void *)pr->info.codep);
-        else if ((pr->tag & 0001) != TAGO)
+        else if ((pr->tag & 0001) != 0)
             rfabe("rfpex: unknown bracket type");
         else
             printf("'%x,%p'", pr->tag, (void *)pr->info.codep);
@@ -455,7 +455,7 @@ void rfpexs(const char *pt, const T_LINKCB *pr, const T_LINKCB *pn, const bool n
         }
         else if (pr->tag == TAGR)
             printf("%%%p", (void *)pr->info.codep);
-        else if ((pr->tag & 0001) != TAGO)
+        else if ((pr->tag & 0001) != 0)
             rfabe("rfpexs: unknown bracket type");
         else
             printf("%x,%p", pr->tag, (void *)pr->info.codep);
@@ -518,7 +518,7 @@ void rfpexm(const char *pt, const T_LINKCB *pr, const T_LINKCB *pn, const bool n
             }
             else if (pr->tag == TAGR)
                 printf("/%%%p/", (void *)pr->info.codep);
-            else if ((pr->tag & 0001) != TAGO)
+            else if ((pr->tag & 0001) != 0)
                 rfabe("rfpexm: unknown bracket type");
             else
                 printf("/%x,%p/", pr->tag, (void *)pr->info.codep);
