749c9dbe9447175cee8f1d25f2b16f7005d43c6c
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-04 19:05:47 +0300

    Add files via upload

--- src/inter/copy_obj.bat
+++ src/inter/copy_obj.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-11-23
+rem 2025-10-05
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 rem *******************************************************************
@@ -11,5 +11,5 @@ rem copy_obj.bat
 rem copy mainrf.o and rfdbg.o
 rem *******************************************************************
 
-copy /y src\inter\mainrf.o lib\
-copy /y src\inter\rfdbg.o lib\
+copy /y src\inter\mainrf.o lib\obj
+copy /y src\inter\rfdbg.o lib\obj
