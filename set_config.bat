5f526b5fd53ee6a0b00906675239177efbb2491e
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-11 19:51:57 +0300

    Add files via upload

---
+++ set_config.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Copyright 2025 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2025-10-11
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
+rem *******************************************************************
+rem set_config.bat
+rem set environments variables for make RefalAB
+rem *******************************************************************
+
+rem set REFALABMCOMP gcc
+set REFALABMCOMP clang
