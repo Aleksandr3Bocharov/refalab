a02521750be43b122f2ad2723a84cb35c1a4f093
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-15 20:18:48 +0300

    Add files via upload

--- src/inter/d.c
+++ src/inter/d.c
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 // 2025-09-23
-// https://github.com/Aleksandr3Bocharov/RefalAB
+// https://github.com/Aleksandr3Bocharov/refalab
 
 //----------- file -- D.C ------------------
 //       RefalAB debugger functions
--- src/inter/rfrun.c
+++ src/inter/rfrun.c
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 // 2025-10-03
-// https://github.com/Aleksandr3Bocharov/RefalAB
+// https://github.com/Aleksandr3Bocharov/refalab
 
 //----------- file RFRUN.C -------------------
 //      Refal-interpretator
