7f9df6b4300d1215860040f98837d3c5677bb3c9
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-15 11:56:09 +0300

    Add files via upload

---
+++ src/comp/refxcv.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Copyright 2025 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2025-10-14
+rem https://github.com/Aleksandr3Bocharov/RefalAB
+
+rem *******************************************************************
+rem refxcv.bat
+rem special script
+rem compile xcv.ref from refal to object file
+rem result: xcv.o  
+rem *******************************************************************
+
+bin\RefalAB src\inter\xcv -a,"-o src\inter\xcv.o" > src\inter\xcv.log
