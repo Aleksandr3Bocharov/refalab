237cd3dabf55d7a2c164b417bfaa6e38f2115365
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-30 15:24:26 +0200

    Add files via upload

---
+++ src/inter/refxar2.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem Copyright 2025 Aleksandr Bocharov
+rem Distributed under the Boost Software License, Version 1.0.
+rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+rem 2025-10-30
+rem https://github.com/Aleksandr3Bocharov/refalab
+
+rem *******************************************************************
+rem refxar2.bat
+rem special script
+rem compile xar2.ref from refal to object file
+rem result: xar2.o  
+rem *******************************************************************
+
+bin\refalabc src\inter\xar2 -a,"-o src\inter\xar2.o" > src\inter\xar2.log
