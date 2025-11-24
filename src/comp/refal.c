d9654f62d758883eb66abc915f4b41fe3da02bd7
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-11-24 21:14:14 +0200

    Add files via upload

--- src/comp/refal.c
+++ src/comp/refal.c
@@ -1,7 +1,7 @@
 // Copyright 2025 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-11-12
+// 2025-11-25
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //-----------  file  --  REFAL.C -------------
@@ -159,7 +159,7 @@ FILE *systxt; // for module names
 uint32_t nommod;
 
 // Aleksandr Bocharov   // compiler version
-static const char *vers_i = "RefalAB Version 1.2 20251112 (c) Aleksandr Bocharov";
+static const char *vers_i = "RefalAB Version 1.3-dev 20251125 (c) Aleksandr Bocharov";
 
 static FILE *sysin;
 static size_t m;           // current symbol number
