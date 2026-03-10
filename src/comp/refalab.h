ce0c1b9c449abfdbb2a2fc73321e2c2ced733000
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-03-10 13:37:13 +0200

    Add files via upload

--- src/comp/refalab.h
+++ src/comp/refalab.h
@@ -1,7 +1,7 @@
-// Copyright 2025 Aleksandr Bocharov
+// Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-09-09
+// 2026-03-10
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //--------------- file -- REFALAB.H -----------------
@@ -25,7 +25,7 @@
 #define Wbin "wb"
 #define Rbin "rb"
 
-#define ZBLL sizeof(uint16_t)
+#define ZBLL 2 // sizeof(uint16_t)
 #define LBLL sizeof(void *)
 #define SMBL (ZBLL + LBLL)
 
