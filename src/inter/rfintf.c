5436737f9c627057b168d1fed041e6f3f8fab4c3
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-03-10 14:43:01 +0200

    Add files via upload

--- src/inter/rfintf.c
+++ src/inter/rfintf.c
@@ -1,7 +1,7 @@
-// Copyright 2025 Aleksandr Bocharov
+// Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2025-10-03
+// 2026-03-10
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //-----------  file  --  RFINTF.C ------------------
