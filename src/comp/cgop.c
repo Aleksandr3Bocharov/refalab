3c0c43b3a908a9de096f4450d0acf08b159c2f0b
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-03-25 16:45:49 +0200

    Add files via upload

--- src/comp/cgop.c
+++ src/comp/cgop.c
@@ -1,7 +1,7 @@
 // Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2026-03-11
+// 2026-03-25
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //------------------file----CGOP.C--------------------
@@ -16,8 +16,8 @@
 
 typedef struct _TAG
 {
-    uint8_t b1;
-    uint8_t b2;
+    uint8_t byte1;
+    uint8_t byte2;
 } T_TAG;
 
 void gopn(uint8_t k, uint8_t n)
@@ -44,26 +44,26 @@ void gopl(uint8_t k, const uint8_t *l)
 
 void gsymbol(const T_LINKTI *code)
 {
-    const T_TAG *q = (T_TAG *)&(code->tag);
-    jbyte(q->b1);
-    jbyte(q->b2);
+    const T_TAG *code_tag = (T_TAG *)&(code->tag);
+    jbyte(code_tag->byte1);
+    jbyte(code_tag->byte2);
     if (code->tag == TAGF)
     {
         j3addr((T_U *)(void *)code->info.codef);
         return;
     };
-    const uint8_t *r = (uint8_t *)&(code->info.codef);
+    const uint8_t *code_info = (uint8_t *)&(code->info.codef);
     if (code->tag == TAGO)
     {
-        jbyte(*r);
-        for (size_t i = 1; i < LBLL; i++)
+        jbyte(*code_info);
+        for (uint8_t i = 1; i < LBLL; i++)
             jbyte(0);
     }
     else
     {
-        size_t i = 0;
+        uint8_t i = 0;
         for (; i < 4 /* sizeof(uint32_t) */; i++)
-            jbyte(*(r + i));
+            jbyte(*(code_info + i));
         for (; i < LBLL; i++)
             jbyte(0);
     }
