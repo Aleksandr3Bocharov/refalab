37870b64c59fda718c5ba757135cdc83199ec9be
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-04-13 08:45:05 +0300

    Add files via upload

--- src/inter/refalab.h
+++ src/inter/refalab.h
@@ -78,10 +78,10 @@ typedef struct linkcb_
 
 // ------------ end of pragma ------------------------------------
 
-typedef struct st_
+typedef struct status_table_
 {
-    struct st_ *stprev;
-    struct st_ *stnext;
+    struct status_table_ *stprev;
+    struct status_table_ *stnext;
     uint32_t state;
     T_LINKCB *dot;
     uint32_t step;
