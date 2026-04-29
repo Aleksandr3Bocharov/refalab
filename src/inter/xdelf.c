d740b9eed118240a3a08283bba81d685a26e373a
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-04-29 20:01:35 +0300

    Add files via upload

--- src/inter/xdelf.c
+++ src/inter/xdelf.c
@@ -1,7 +1,7 @@
 // Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2026-04-14
+// 2026-04-28
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //-------------- file -- XDELF.C ------------
@@ -20,33 +20,32 @@ static void delf_(void)
         refal.upshot = 2;
         return;
     } // FAIL
-    const T_LINKCB *dot = refal.next_argument;
-    const T_LINKCB *dot1 = refal.next_result->info.codep;
-    const T_LINKCB *sk;
-    T_LINKCB *nd;
+    const T_LINKCB *delete_dot = refal.next_argument;
+    const T_LINKCB *temp_k;
+    const T_LINKCB *delete_dot_next;
     while (true)
     {
-        sk = dot->info.codep;
-        dot = sk->info.codep;
-        if (dot == NULL)
+        temp_k = delete_dot->info.codep;
+        delete_dot = temp_k->info.codep;
+        if (delete_dot == NULL)
             refal_abort_end("delf: sign '#' missing");
-        nd = dot->next;
-        if (nd->info.infoc != '#')
+        delete_dot_next = delete_dot->next;
+        if (delete_dot_next->info.infoc != '#')
             continue;
         break;
     }
-    while (dot1 != dot)
+    const T_LINKCB *current_delete_dot = refal.next_result->info.codep;
+    while (current_delete_dot != delete_dot)
     {
-        sk = dot1->info.codep;
-        nd = sk->info.codep;
-        insert_to_free_memory_list(sk->previous, dot1->next);
-        dot1 = nd;
+        temp_k = current_delete_dot->info.codep;
+        const T_LINKCB *temp_dot = temp_k->info.codep;
+        insert_to_free_memory_list(temp_k->previous, current_delete_dot->next);
+        current_delete_dot = temp_dot;
     }
-    sk = dot1->info.codep;
-    nd = sk->info.codep;
-    dot1 = dot1->next;
-    insert_to_free_memory_list(sk->previous, dot1->next);
-    refal.next_result->info.codep = nd;
+    temp_k = current_delete_dot->info.codep;
+    T_LINKCB *temp_dot = temp_k->info.codep;
+    insert_to_free_memory_list(temp_k->previous, delete_dot_next->next);
+    refal.next_result->info.codep = temp_dot;
     return;
 }
 char delf_0[] = {Z4 'D', 'E', 'L', 'F', (char)4};
