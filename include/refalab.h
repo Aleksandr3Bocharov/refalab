d9d64ebe6c3332f860a7a2e65c1f565f5e9ed385
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-01 19:31:10 +0300

    Add files via upload

--- include/refalab.h
+++ include/refalab.h
@@ -104,7 +104,7 @@ typedef struct refal
     T_LINKCB *previous_result;
     T_LINKCB *next_result;
     T_STATUS_TABLE *current_status_table;
-    T_LINKCB *free_memory_list_head;
+    T_LINKCB *free_memory_head;
     T_LINKCB *static_boxes;
     T_LINKCB *dynamic_boxes;
     struct
@@ -124,7 +124,7 @@ extern T_REFAL refal;
 
 extern void refal_abort_end(const char *abort_message);
 extern void delete_status_table(const T_STATUS_TABLE *status_table);
-extern void insert_to_free_memory_list(T_LINKCB *before, T_LINKCB *after);
+extern void insert_to_free_memory(T_LINKCB *before, T_LINKCB *after);
 extern void refal_execute(uint8_t *refalab_function);
 extern void refal_get_args(int argc, char *argv[]);
 extern T_LINKCB *get_string_expression(char *string, size_t max_string_size, T_LINKCB *begin, const T_LINKCB *max_after);
@@ -141,11 +141,11 @@ extern bool copy_expression(T_LINKCB *where, T_LINKCB *before, T_LINKCB *after);
 extern bool create_status_table(T_STATUS_TABLE *status_table);
 extern bool exist_status_table(const T_STATUS_TABLE *status_table);
 extern bool more_free_memory(void);
-extern bool insert_from_free_memory_list(T_LINKCB *where, size_t count);
-extern bool extended_insert_from_free_memory_list(T_LINKCB *where, size_t count);
+extern bool insert_from_free_memory(T_LINKCB *where, size_t count);
+extern bool extended_insert_from_free_memory(T_LINKCB *where, size_t count);
 extern bool insert_view_k_function_dot(T_STATUS_TABLE *status_table, uint8_t *refalab_function);
 extern T_LINKCB *find_duplicate(const T_LINKCB *before, const T_LINKCB *after, const T_LINKCB *before_duplicate);
-extern bool check_count_free_memory_list(size_t count);
+extern bool check_count_free_memory(size_t count);
 
 extern void refal_run(T_STATUS_TABLE *status_table);
 
