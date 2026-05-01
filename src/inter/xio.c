aecb4485dd7752c256e36fbbcbc338c8a81f68ca
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-01 08:49:42 +0300

    Add files via upload

--- src/inter/xio.c
+++ src/inter/xio.c
@@ -1,7 +1,7 @@
 // Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2026-03-14
+// 2026-05-01
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //-------------- file -- XIO.C ------------
@@ -18,22 +18,22 @@ static void card_(void)
 {
     if (refal.previous_argument->next != refal.next_argument)
         print_expression_s("", refal.previous_argument, refal.next_argument, false);
-    T_LINKCB *p = refal.previous_result;
-    int c = getchar();
-    while (c != '\n')
+    T_LINKCB *current_result = refal.previous_result;
+    int get_result = getchar();
+    while (get_result != '\n')
     {
-        if  (!extended_insert_from_free_memory_list(p, 1))
+        if (!extended_insert_from_free_memory_list(current_result, 1))
             return;
-        p = p->next;
-        p->info.code = NULL;
-        if (c == EOF)
+        current_result = current_result->next;
+        current_result->info.code = NULL;
+        if (get_result == EOF)
         {
-            p->tag = TAGN;
+            current_result->tag = TAGN;
             return;
         }
-        p->tag = TAGO;
-        p->info.infoc = (char)c;
-        c = getchar();
+        current_result->tag = TAGO;
+        current_result->info.infoc = (char)get_result;
+        get_result = getchar();
     }
     return;
 }
@@ -41,118 +41,118 @@ char card_0[] = {Z4 'C', 'A', 'R', 'D', (char)4};
 G_L_B uint8_t refalab_card = '\122';
 void (*card_1)(void) = card_;
 
-static void pr1_(void)
+static void print_(void)
 {
     print_expression("", refal.previous_argument, refal.next_argument, true);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char pr1_0[] = {Z5 'P', 'R', 'I', 'N', 'T', (char)5};
+char print_0[] = {Z5 'P', 'R', 'I', 'N', 'T', (char)5};
 G_L_B uint8_t refalab_print = '\122';
-void (*pr1_1)(void) = pr1_;
+void (*print_1)(void) = print_;
 
-static void pr2_(void)
+static void prints_(void)
 {
     print_expression_s("", refal.previous_argument, refal.next_argument, true);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char pr2_0[] = {Z6 'P', 'R', 'I', 'N', 'T', 'S', (char)6};
+char prints_0[] = {Z6 'P', 'R', 'I', 'N', 'T', 'S', (char)6};
 G_L_B uint8_t refalab_prints = '\122';
-void (*pr2_1)(void) = pr2_;
+void (*prints_1)(void) = prints_;
 
-static void pr3_(void)
+static void printm_(void)
 {
     print_expression_m("", refal.previous_argument, refal.next_argument, true);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char pr3_0[] = {Z6 'P', 'R', 'I', 'N', 'T', 'M', (char)6};
+char printm_0[] = {Z6 'P', 'R', 'I', 'N', 'T', 'M', (char)6};
 G_L_B uint8_t refalab_printm = '\122';
-void (*pr3_1)(void) = pr3_;
+void (*printm_1)(void) = printm_;
 
-static void pr4_(void)
+static void prout_(void)
 {
     print_expression("", refal.previous_argument, refal.next_argument, true);
     return;
 }
-char pr4_0[] = {Z5 'P', 'R', 'O', 'U', 'T', (char)5};
+char prout_0[] = {Z5 'P', 'R', 'O', 'U', 'T', (char)5};
 G_L_B uint8_t refalab_prout = '\122';
-void (*pr4_1)(void) = pr4_;
+void (*prout_1)(void) = prout_;
 
-static void pr5_(void)
+static void prouts_(void)
 {
     print_expression_s("", refal.previous_argument, refal.next_argument, true);
     return;
 }
-char pr5_0[] = {Z6 'P', 'R', 'O', 'U', 'T', 'S', (char)6};
+char prouts_0[] = {Z6 'P', 'R', 'O', 'U', 'T', 'S', (char)6};
 G_L_B uint8_t refalab_prouts = '\122';
-void (*pr5_1)(void) = pr5_;
+void (*prouts_1)(void) = prouts_;
 
-static void pr6_(void)
+static void proutm_(void)
 {
     print_expression_m("", refal.previous_argument, refal.next_argument, true);
     return;
 }
-char pr6_0[] = {Z6 'P', 'R', 'O', 'U', 'T', 'M', (char)6};
+char proutm_0[] = {Z6 'P', 'R', 'O', 'U', 'T', 'M', (char)6};
 G_L_B uint8_t refalab_proutm = '\122';
-void (*pr6_1)(void) = pr6_;
+void (*proutm_1)(void) = proutm_;
 
-static void wr1_(void)
+static void write_(void)
 {
     print_expression("", refal.previous_argument, refal.next_argument, false);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char wr1_0[] = {Z5 'W', 'R', 'I', 'T', 'E', (char)5};
+char write_0[] = {Z5 'W', 'R', 'I', 'T', 'E', (char)5};
 G_L_B uint8_t refalab_write = '\122';
-void (*wr1_1)(void) = wr1_;
+void (*write_1)(void) = write_;
 
-static void wr2_(void)
+static void writes_(void)
 {
     print_expression_s("", refal.previous_argument, refal.next_argument, false);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char wr2_0[] = {Z6 'W', 'R', 'I', 'T', 'E', 'S', (char)6};
+char writes_0[] = {Z6 'W', 'R', 'I', 'T', 'E', 'S', (char)6};
 G_L_B uint8_t refalab_writes = '\122';
-void (*wr2_1)(void) = wr2_;
+void (*writes_1)(void) = writes_;
 
-static void wr3_(void)
+static void writem_(void)
 {
     print_expression_m("", refal.previous_argument, refal.next_argument, false);
     transplantation(refal.previous_result, refal.previous_argument, refal.next_argument);
     return;
 }
-char wr3_0[] = {Z6 'W', 'R', 'I', 'T', 'E', 'M', (char)6};
+char writem_0[] = {Z6 'W', 'R', 'I', 'T', 'E', 'M', (char)6};
 G_L_B uint8_t refalab_writem = '\122';
-void (*wr3_1)(void) = wr3_;
+void (*writem_1)(void) = writem_;
 
-static void wr4_(void)
+static void wrout_(void)
 {
     print_expression("", refal.previous_argument, refal.next_argument, false);
     return;
 }
-char wr4_0[] = {Z5 'W', 'R', 'O', 'U', 'T', (char)5};
+char wrout_0[] = {Z5 'W', 'R', 'O', 'U', 'T', (char)5};
 G_L_B uint8_t refalab_wrout = '\122';
-void (*wr4_1)(void) = wr4_;
+void (*wrout_1)(void) = wrout_;
 
-static void wr5_(void)
+static void wrouts_(void)
 {
     print_expression_s("", refal.previous_argument, refal.next_argument, false);
     return;
 }
-char wr5_0[] = {Z6 'W', 'R', 'O', 'U', 'T', 'S', (char)6};
+char wrouts_0[] = {Z6 'W', 'R', 'O', 'U', 'T', 'S', (char)6};
 G_L_B uint8_t refalab_wrouts = '\122';
-void (*wr5_1)(void) = wr5_;
+void (*wrouts_1)(void) = wrouts_;
 
-static void wr6_(void)
+static void wroutm_(void)
 {
     print_expression_m("", refal.previous_argument, refal.next_argument, false);
     return;
 }
-char wr6_0[] = {Z6 'W', 'R', 'O', 'U', 'T', 'M', (char)6};
+char wroutm_0[] = {Z6 'W', 'R', 'O', 'U', 'T', 'M', (char)6};
 G_L_B uint8_t refalab_wroutm = '\122';
-void (*wr6_1)(void) = wr6_;
+void (*wroutm_1)(void) = wroutm_;
 
 //------------------ end of file  XIO.C ----------------
