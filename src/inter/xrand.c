f50495cbe68554826bac46fe276d27f533fd3240
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-04 08:20:06 +0300

    Add files via upload

--- src/inter/xrand.c
+++ src/inter/xrand.c
@@ -19,38 +19,38 @@
 #define MIN_DELAY 33
 #define MAX_DELAY 97
 
-static bool init = false;
+static bool randomizer_init = false;
 static uint8_t n_sub_max_delay = MAX_DELAY - 1;
 static uint8_t n_sub_min_delay = MIN_DELAY - 1;
-static uint32_t y[MAX_DELAY];
+static uint32_t x[MAX_DELAY];
 
 static uint32_t random_number(void)
 {
-    if (!init)
+    if (!randomizer_init)
     {
         srand((unsigned int)time(NULL));
         for(uint8_t i = 0; i < MAX_DELAY; i++)
-            y[i] = (uint32_t)((double)rand() / RAND_MAX * MAX_NUMBER);
-        init = true;
+            x[i] = (uint32_t)((double)rand() / RAND_MAX * MAX_NUMBER);
+        randomizer_init = true;
     }
-    y[n_sub_max_delay] += y[n_sub_min_delay];
-    const uint32_t result = y[n_sub_max_delay];
+    x[n_sub_max_delay] += x[n_sub_min_delay];
+    const uint32_t number = x[n_sub_max_delay];
     n_sub_max_delay = (n_sub_max_delay + MAX_DELAY - 1) % MAX_DELAY;
     n_sub_min_delay = (n_sub_min_delay + MAX_DELAY - 1) % MAX_DELAY;
-    return result;
+    return number;
 }
 
 static uint32_t random_number_in_range(uint32_t limit)
 {
     if (limit == 0)
         return 0;
-    const uint32_t max_valid = MAX_NUMBER - MAX_NUMBER % limit;
-    uint32_t random;
+    const uint32_t max_number_valid = MAX_NUMBER - MAX_NUMBER % limit;
+    uint32_t number_random;
     do
     {
-        random = random_number();
-    } while (random >= max_valid);
-    return random % limit;
+        number_random = random_number();
+    } while (number_random >= max_number_valid);
+    return number_random % limit;
 }
 
 static void random_(void)
