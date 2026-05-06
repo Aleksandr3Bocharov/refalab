05b679be823dc7034c662c582d66454f25617834
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-06 14:54:56 +0300

    Add files via upload

--- src/inter/xtry.c
+++ src/inter/xtry.c
@@ -1,7 +1,7 @@
 // Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2026-04-14
+// 2026-05-05
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //-----------  file  --  XTRY.C --------------
@@ -18,104 +18,103 @@
 
 static void try_(void)
 {
-    T_STATUS_TABLE *s_st;
-    size_t l = (size_t)&s_st;
-    T_STATUS_TABLE *upst;
-    T_LINKCB *px;
-    bool lack = false;
-    if ((l & 0xffff) < 200)
-        lack = true;
+    T_STATUS_TABLE *new_status_table;
+    size_t new_status_table_pointer_number = (size_t)&new_status_table;
+    bool lack_memory = false;
+    if ((new_status_table_pointer_number & 0xffff) < 200) //??????????????????????
+        lack_memory = true;
     else
     {
-        upst = refal.current_status_table;
-        if (!extended_insert_from_free_memory(refal.previous_result, 1))
-            return;
-        px = refal.previous_result->next;
-        s_st = malloc(sizeof(T_STATUS_TABLE));
-        if (s_st == NULL)
-            lack = true;
-        else if (!create_status_table(s_st))
-            lack = true;
-        else if (!insert_from_free_memory(s_st->view, 2))
+        new_status_table = malloc(sizeof(T_STATUS_TABLE));
+        if (new_status_table == NULL)
+            lack_memory = true;
+        else if (!create_status_table(new_status_table))
+            lack_memory = true;
+        else if (!extended_insert_from_free_memory(new_status_table->view, 2))
         {
-            delete_status_table(s_st);
-            lack = true;
+            delete_status_table(new_status_table);
+            return;
         }
     }
-    if (lack)
+    if (lack_memory)
     {
         refal.upshot = 3;
         return;
     }
-    T_LINKCB *pk = s_st->view->next;
-    T_LINKCB *pd = pk->next;
-    pk->info.code = NULL;
-    pk->tag = TAGK;
-    pd->info.codep = pk;
-    pd->tag = TAGD;
-    s_st->dot = pd;
-    transplantation(pk, refal.previous_argument, refal.next_argument);
-    transplantation(s_st->store, upst->store, upst->store);
-    s_st->step = ++upst->step;
-    s_st->stop = MAX_STOP;
+    T_LINKCB *k = new_status_table->view->next;
+    T_LINKCB *dot = k->next;
+    k->info.code = NULL;
+    k->tag = TAGK;
+    dot->info.codep = k;
+    dot->tag = TAGD;
+    new_status_table->dot = dot;
+    transplantation(k, refal.previous_argument, refal.next_argument);
+    T_STATUS_TABLE *current_status_table = refal.current_status_table;
+    transplantation(new_status_table->store, current_status_table->store, current_status_table->store);
+    new_status_table->step = ++current_status_table->step;
+    new_status_table->stop = MAX_STOP;
 #if defined mdebug
-    const uint32_t s_stop = s_st->stop;
+    const uint32_t step_stop = new_status_table->stop;
 #endif
     do
     {
         if (status_table_debugger == NULL)
         {
 #if defined mdebug
-            if (s_st->step >= s_stop)
-                s_st->step = 0;
-            s_st->stop = s_st->step + 1;
-            const T_LINKCB *pk1 = s_st->dot->info.codep;
-            const T_LINKCB *prevk = pk1->previous;
-            const T_LINKCB *nextd = s_st->dot->next;
-            printf(" Step: %d\n", s_st->stop);
-            print_expression_m(" Term: ", prevk, nextd, true);
-            refal_run(s_st);
-            if (s_st->state == 1)
-                print_expression_m(" Result: ", prevk, nextd, true);
+            if (new_status_table->step >= step_stop)
+                new_status_table->step = 0;
+            new_status_table->stop = new_status_table->step + 1;
+            const T_LINKCB *current_k = new_status_table->dot->info.codep;
+            const T_LINKCB *previous_current_k = current_k->previous;
+            const T_LINKCB *next_current_dot = new_status_table->dot->next;
+            printf(" Step: %d\n", new_status_table->stop);
+            print_expression_m(" Term: ", previous_current_k, next_current_dot, true);
+            refal_run(new_status_table);
+            if (new_status_table->state == 1)
+                print_expression_m(" Result: ", previous_current_k, next_current_dot, true);
 #else
-            refal_run(s_st); // net prokrutki
-            if (s_st->state == 1 && s_st->dot != NULL)
+            refal_run(new_status_table); // net prokrutki
+            if (new_status_table->state == 1 && new_status_table->dot != NULL)
             {
-                s_st->step = 0;
+                new_status_table->step = 0;
                 continue;
             }
 #endif
         }
         else
-            (*status_table_debugger)(s_st); // prokrutka vkluchena
-        if (s_st->state == 3)
+            (*status_table_debugger)(new_status_table); // prokrutka vkluchena
+        if (new_status_table->state == 3)
             if (more_free_memory())
-                s_st->state = 1;
-    } while (s_st->state == 1 && s_st->dot != NULL);
-    transplantation(upst->store, s_st->store, s_st->store);
-    upst->step = --s_st->step;
+                new_status_table->state = 1;
+    } while (new_status_table->state == 1 && new_status_table->dot != NULL);
+    transplantation(current_status_table->store, new_status_table->store, new_status_table->store);
+    current_status_table->step = --new_status_table->step;
 #if defined mdebug
     if (status_table_debugger == NULL)
-        upst->stop = upst->step + 1;
+        current_status_table->stop = current_status_table->step + 1;
 #endif
-    switch (s_st->state)
+    transplantation(refal.previous_result, refal.previous_argument->previous, refal.previous_argument->next);
+    T_LINKCB *symbol_char_result = refal.previous_result->next;
+    symbol_char_result->tag = TAGO;
+    symbol_char_result->info.code = NULL;
+    switch (new_status_table->state)
     {
     case 1:
-        px->info.infoc = 'N';
-        transplantation(px, s_st->view, s_st->view);
+        symbol_char_result->info.infoc = 'N';
+        transplantation(symbol_char_result, new_status_table->view, new_status_table->view);
         break;
     case 2:
-        px->info.infoc = 'R';
-        pd = s_st->dot;
-        pk = pd->info.codep;
-        transplantation(px, pk, pd);
+        symbol_char_result->info.infoc = 'R';
+        dot = new_status_table->dot;
+        k = dot->info.codep;
+        transplantation(symbol_char_result, k, dot);
         break;
     case 3:
-        px->info.infoc = 'S';
+        symbol_char_result->info.infoc = 'S';
         break;
     }
-    delete_status_table(s_st);
-    free(s_st);
+    delete_status_table(new_status_table);
+    free(new_status_table);
     return;
 }
 char try_0[] = {Z3 'T', 'R', 'Y', (char)3};
