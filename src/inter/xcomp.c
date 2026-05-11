0b0f1953c8e47be5534e1bfcdbc42af78021ba2a
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-11 11:39:47 +0300

    Add files via upload

--- src/inter/xcomp.c
+++ src/inter/xcomp.c
@@ -1,7 +1,7 @@
 // Copyright 2026 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2026-04-28
+// 2026-05-11
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //------------ file -- XCOMP.C ---------------
@@ -40,6 +40,32 @@ char nrel_0[] = {Z4 'N', 'R', 'E', 'L', (char)4};
 G_L_B uint8_t refalab_nrel = '\122';
 void (*nrel_1)(void) = nrel_;
 
+static uint8_t compare_expressions_lexicographic(const T_LINKCB *before, const T_LINKCB *middle, const T_LINKCB *after)
+{
+    const T_LINKCB *first_current = before->next;
+    const T_LINKCB *second_current = middle->next;
+    for (; first_current != middle && second_current != after; first_current = first_current->next, second_current = second_current->next)
+        if ((first_current->tag == TAGLB && second_current->tag == TAGLB) || (first_current->tag == TAGRB && second_current->tag == TAGRB))
+            continue;
+        else if (first_current->tag == TAGLB || second_current->tag == TAGRB)
+            return 0; //X>Y
+        else if (first_current->tag == TAGRB || second_current->tag == TAGLB)
+            return 1; //X<Y
+        else if (first_current->tag > second_current->tag)
+            return 0; //X>Y
+        else if (first_current->tag < second_current->tag)
+            return 1; //X<Y
+        else if ((size_t)first_current->info.code > (size_t)second_current->info.code)
+            return 0; //X>Y
+        else if ((size_t)first_current->info.code < (size_t)second_current->info.code)
+            return 1; //X<Y
+    if (first_current == middle && second_current != after)
+        return 1; //X<Y
+    if (second_current == after && first_current != middle)
+        return 0; //X>Y
+    return 2; //X=Y
+}
+
 static void lrel_(void)
 {
     const T_LINKCB *first_begin = refal.previous_argument->next;
@@ -49,32 +75,12 @@ static void lrel_(void)
         return;
     }
     const T_LINKCB *second_begin = first_begin->info.codep;
-    const T_LINKCB *first_current = first_begin->next;
-    const T_LINKCB *second_current = second_begin->next;
     char compare_result = '=';
-    for (; compare_result == '=' && first_current != second_begin && second_current != refal.next_argument; first_current = first_current->next, second_current = second_current->next)
-        if ((first_current->tag == TAGLB && second_current->tag == TAGLB) ||
-            (first_current->tag == TAGRB && second_current->tag == TAGRB))
-            continue;
-        else if (first_current->tag == TAGLB || second_current->tag == TAGRB)
-            compare_result = '>';
-        else if (first_current->tag == TAGRB || second_current->tag == TAGLB)
-            compare_result = '<';
-        else if (first_current->tag > second_current->tag)
-            compare_result = '>';
-        else if (first_current->tag < second_current->tag)
-            compare_result = '<';
-        else if ((size_t)first_current->info.code > (size_t)second_current->info.code)
-            compare_result = '>';
-        else if ((size_t)first_current->info.code < (size_t)second_current->info.code)
-            compare_result = '<';
-    if (compare_result == '=')
-    {
-        if (first_current == second_begin && second_current != refal.next_argument)
-            compare_result = '<';
-        else if (second_current == refal.next_argument && first_current != second_begin)
-            compare_result = '>';
-    }
+    const uint8_t compare = compare_expressions_lexicographic(first_begin, second_begin, refal.next_argument);
+    if (compare == 0)
+        compare_result = '>';
+    else if (compare == 1)
+        compare_result = '<';
     refal.previous_argument->tag = TAGO;
     refal.previous_argument->info.code = NULL;
     refal.previous_argument->info.infoc = compare_result;
