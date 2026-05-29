e9f55b15380c25263617ded19559d9bbb9f2b253
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-05-29 22:08:32 +0300

    Add files via upload

--- src/inter/copy_obj.bat
+++ src/inter/copy_obj.bat
@@ -1,9 +1,8 @@
 @echo off
 
-rem Copyright 2025 Aleksandr Bocharov
-rem Distributed under the Boost Software License, Version 1.0.
-rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2025-10-05
+rem Copyright (c) 2026 Aleksandr Bocharov
+rem SPDX-License-Identifier: MIT
+rem 2026-05-29
 rem https://github.com/Aleksandr3Bocharov/refalab
 
 rem *******************************************************************
--- src/inter/refxsys2.bat
+++ src/inter/refxsys2.bat
@@ -1,9 +1,8 @@
 @echo off
 
-rem Copyright 2025 Aleksandr Bocharov
-rem Distributed under the Boost Software License, Version 1.0.
-rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2025-11-03
+rem Copyright (c) 2026 Aleksandr Bocharov
+rem SPDX-License-Identifier: MIT
+rem 2026-05-29
 rem https://github.com/Aleksandr3Bocharov/refalab
 
 rem *******************************************************************
