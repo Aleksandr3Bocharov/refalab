9b0ba096c4bdd1cb575446eafdfee0d803048168
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-06-03 07:50:44 +0300

    Add files via upload

---
+++ del_env.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Copyright (c) 2026 Aleksandr Bocharov
+rem SPDX-License-Identifier: MIT
+rem 2026-05-31
+rem https://github.com/Aleksandr3Bocharov/refalab
+
+rem *******************************************************************
+rem del_env.bat
+rem delete environment variables for RefalAB
+rem *******************************************************************
+
+
+pause
+exit /b 0
