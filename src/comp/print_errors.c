da3a410d36f931abfbd4403e135bfc5eeb8fe55a
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-06-03 16:28:39 +0300

    Update print_errors.c

--- src/comp/print_errors.c
+++ src/comp/print_errors.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 Aleksandr Bocharov
 // SPDX-License-Identifier: MIT
-// 2026-05-29
+// 2026-06-05
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //----------  file print_errors.c  ---------- 
