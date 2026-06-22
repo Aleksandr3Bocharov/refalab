063cf90ddafde4f497a95cd018addfb2b4d23413
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-06-22 15:39:46 +0300

    Add files via upload

--- src/comp/print_errors.c
+++ src/comp/print_errors.c
@@ -8,6 +8,7 @@
 //------------------------------------------- 
 
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include "refalab.h"
 #include "print_errors.h"
@@ -18,7 +19,7 @@ void print_error_string(uint16_t error_number, const char *error_string)
     char tmp[512];
     size_t line, column;
     get_location(&line, &column, scanner.last_error_cursor);
-    sprintf(tmp, "***** Line %zuб Column %zu: [ERROR %u] %s\n", line, column, error_number, error_string);
+    sprintf(tmp, "***** Line %zu, Column %zu: [ERROR %u] %s\n", line, column, error_number, error_string);
     if (refalab_source_listing != NULL)
         fputs(tmp, refalab_source_listing);
     fputs(tmp, terminal);
@@ -28,42 +29,46 @@ void print_error_string(uint16_t error_number, const char *error_string)
 
 void print_error_two_strings(uint16_t error_number, const char *error_string, const char *error2_string, uint8_t error2_string_length)
 {
-    processing_error();
     char tmp1[512];
-    for (uint8_t i = 0; i < error2_string_length; i++)
-        tmp1[i] = *(error2_string + i);
+    memcpy(tmp1, error2_string, error2_string_length);
     tmp1[error2_string_length] = '\0';
     char tmp[512];
-    sprintf(tmp, "***** %s %s\n", error_string, tmp1);
+    size_t line, column;
+    get_location(&line, &column, scanner.last_error_cursor);
+    sprintf(tmp, "***** Line %zu, Column %zu: [ERROR %u] %s %s\n", line, column, error_number, error_string, tmp1);
     if (refalab_source_listing != NULL)
         fputs(tmp, refalab_source_listing);
     fputs(tmp, terminal);
+    processing_error();
     return;
 }
 
 void print_error_three_strings(uint16_t error_number, const char *error_string, const char *error2_string, uint8_t error2_string_length, const char *error3_string)
 {
-    processing_error();
     char tmp1[512];
-    for (uint8_t i = 0; i < error2_string_length; i++)
-        tmp1[i] = *(error2_string + i);
+    memcpy(tmp1, error2_string, error2_string_length);
     tmp1[error2_string_length] = '\0';
     char tmp[512];
-    sprintf(tmp, "***** %s %s %s\n", error_string, tmp1, error3_string);
+    size_t line, column;
+    get_location(&line, &column, scanner.last_error_cursor);
+    sprintf(tmp, "***** Line %zu, Column %zu: [ERROR %u] %s %s %s\n", line, column, error_number, error_string, tmp1, error3_string);
     if (refalab_source_listing != NULL)
         fputs(tmp, refalab_source_listing);
     fputs(tmp, terminal);
+    processing_error();
     return;
 }
 
 void print_error_string_symbol(uint16_t error_number, const char *error_string, const char symbol)
 {
-    processing_error();
     char tmp[512];
-    sprintf(tmp, "***** %s %c\n", error_string, symbol);
+    size_t line, column;
+    get_location(&line, &column, scanner.last_error_cursor);
+    sprintf(tmp, "***** Line %zu, Column %zu: [ERROR %u] %s %c\n", line, column, error_number, error_string, symbol);
     if (refalab_source_listing != NULL)
         fputs(tmp, refalab_source_listing);
     fputs(tmp, terminal);
+    processing_error();
     return;
 }
 
