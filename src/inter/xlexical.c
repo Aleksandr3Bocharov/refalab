7792577391a0a05317dd7d991be04b0779ba0ccc
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-07-27 11:59:17 +0300

    Add files via upload

--- src/inter/xlexical.c
+++ src/inter/xlexical.c
@@ -657,4 +657,74 @@ char existw_0[] = {Z6 'E', 'X', 'I', 'S', 'T', 'W', (char)6};
 G_L_B uint8_t refalab_existw = '\122';
 void (*existw_1)(void) = existw_;
 
+static void existn_(void)
+{
+    do
+    {
+        const T_LINKCB *exist_big_number_begin = refal.previous_argument->next;
+        if (exist_big_number_begin->tag != TAGLB)
+            break;
+        const T_LINKCB *exist_big_number_end = exist_big_number_begin->info.codep;
+        T_BIG_NUMBER Exist_Big_Number;
+        if (!read_big_number_expression(&Exist_Big_Number, exist_big_number_begin, exist_big_number_end))
+            break;
+        const T_LINKCB *current_argument_begin = exist_big_number_end->next;
+        bool impossible = false;
+        bool exist_big_number = false;
+        while (current_argument_begin != refal.next_argument)
+        {
+            if (current_argument_begin->tag != TAGLB)
+            {
+                impossible = true;
+                break;
+            }
+            const T_LINKCB *current_argument_end = current_argument_begin->info.codep;
+            T_BIG_NUMBER Current_Big_Number;
+            if (!read_big_number_expression(&Current_Big_Number, current_argument_begin, current_argument_end))
+            {
+                impossible = true;
+                break;
+            }
+            if (compare_big_numbers(&Exist_Big_Number, &Current_Big_Number) == 0)
+            {
+                exist_big_number = true;
+                current_argument_begin = current_argument_end->next;
+                break;
+            }
+            current_argument_begin = current_argument_end->next;
+        }
+        if (impossible)
+            break;
+        while (current_argument_begin != refal.next_argument)
+        {
+            if (current_argument_begin->tag != TAGLB)
+            {
+                impossible = true;
+                break;
+            }
+            const T_LINKCB *current_argument_end = current_argument_begin->info.codep;
+            T_BIG_NUMBER Current_Big_Number;
+            if (!read_big_number_expression(&Current_Big_Number, current_argument_begin, current_argument_end))
+            {
+                impossible = true;
+                break;
+            }
+            current_argument_begin = current_argument_end->next;
+        }
+        if (impossible)
+            break;
+        if (exist_big_number)
+            refal.previous_argument->info.codef = &refalab_true;
+        else
+            refal.previous_argument->info.codef = &refalab_false;
+        transplantation(refal.previous_result, refal.previous_argument->previous, refal.previous_argument->next);
+        return;
+    } while (false);
+    refal.upshot = 2;
+    return;
+}
+char existn_0[] = {Z6 'E', 'X', 'I', 'S', 'T', 'N', (char)6};
+G_L_B uint8_t refalab_existn = '\122';
+void (*existn_1)(void) = existn_;
+
 //----------  end of file xlexical.c  ----------
