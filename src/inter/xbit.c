2408c5e867796027b9f7edbab9cd1bbe5ffe5b95
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-08-06 19:04:19 +0300

    Add files via upload

---
+++ src/inter/xbit.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Aleksandr Bocharov
+// SPDX-License-Identifier: MIT
+// 2026-08-07
+// https://github.com/Aleksandr3Bocharov/refalab
+
+//----------  file xbit.c  ----------
+//                Bitwise MO:
+//      Bit, Bitand, Bitor, Bitxor,
+//      Bitnot, Bitshl, Bitshr,
+//      Bitrol, Bitror
+//-----------------------------------
+
+
+
+//----------  end of file xbit.c  ----------
