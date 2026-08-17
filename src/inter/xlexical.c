5417dfee60f2ea7b01dba8d2334ce8d4b65e9fb0
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2026-08-17 15:08:19 +0300

    Add files via upload

--- src/inter/xlexical.c
+++ src/inter/xlexical.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2026 Aleksandr Bocharov
 // SPDX-License-Identifier: MIT
-// 2026-08-05
+// 2026-08-17
 // https://github.com/Aleksandr3Bocharov/refalab
 
 //----------  file xlexical.c  ----------
